6ee8a94de7a7fd3f29c1b4a38931b0808641064a
Vadym Tsyndra <Mr.Tsyndra@gmail.com>
2023-04-24 09:21:45 +0300

    clone todo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,9 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
-import './App.scss';
-
-interface Props {
-  onClick: () => void;
-}
-
-export const Provider: React.FC<Props> = React.memo(
-  ({ onClick, children }) => (
-    <button
-      type="button"
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  ),
-);
+import { TodoApp } from './components/TodoApp';
 
 export const App: React.FC = () => {
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>
-        <TodoList />
-      </Provider>
-    </div>
+    <TodoApp />
   );
 };
